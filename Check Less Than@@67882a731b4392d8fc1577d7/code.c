7821826cc0474124f961c52e31ae156832367b2b
Abhi-git-dev1 <abhishek0246.becse24@chitkara.edu.in>
2025-02-10 10:32:45 +0530

    Solution to Check Less Than

--- Check Less Than@@67882a731b4392d8fc1577d7/code.c
+++ Check Less Than@@67882a731b4392d8fc1577d7/code.c
@@ -3,11 +3,11 @@
 
 int main() {
    int a,b;
-   scanf("%d %d",&a&b);
+   scanf("%d %d",&a,&b);
    if (a<b){
     printf("True");
    }
    else{
-    printf("Fasle")
+    printf("Fasle");
    }
 }
